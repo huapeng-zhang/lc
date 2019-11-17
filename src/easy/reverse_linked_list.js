32e5da0734861a4f9f33e2334a12c3ada0287fc2
HuapengZhangTS <zhp@cn.tradeshift.com>
2019-11-17 16:39:10 +0800

    reverse linked list

---
+++ src/easy/reverse_linked_list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let cur = head;
+    let next = null;
+
+    while (cur) {
+        next = cur.next;
+        cur.next = prev;
+        prev = cur;
+
+        cur = next;
+    }
+
+    return prev;
+};
