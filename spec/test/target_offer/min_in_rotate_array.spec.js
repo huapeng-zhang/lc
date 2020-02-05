57947445f68e356b6ac274325dc6ed3c821b4ee5
HuapengZhangTS <zhp@cn.tradeshift.com>
2020-02-05 13:55:22 +0800

    find min in rotate array

--- spec/test/target_offer/min_in_rotate_array.spec.js
+++ spec/test/target_offer/min_in_rotate_array.spec.js
@@ -20,13 +20,19 @@ describe('find min number in rotate sorted array', () => {
             input: [[2,1]],
             output: 1
         }, {
+            input: [[1,2,3,4,5]],
+            output: 1
+        }, {
             input: [[10,11,12,13,14,15,5,6,7,8,9]],
             output: 5
         }, {
             input: [[10,11,12,13,14,15,5,6,7,8,9,10]],
             output: 5
         }, {
-            input: [[10,11,12,13,14,15,5,5,5,5,6,7,8,9]],
+            input: [[10,11,12,13,14,15,5,5,5,5,5,5,5,5]],
+            output: 5
+        }, {
+            input: [[10,11,12,13,14,15,5,6,6,6,6,6,6,6]],
             output: 5
         }, {
             input: [[10,11,12,13,14,15,5]],
@@ -34,6 +40,15 @@ describe('find min number in rotate sorted array', () => {
         }, {
             input: [[10,11,12,13,14,15,5,6]],
             output: 5
+        }, {
+            input: [[10,11,12,13,14,15]],
+            output: 10
+        }, {
+            input: [[1,0,1,1,1,]],
+            output: 0
+        }, {
+            input: [[1,1,1,0,1,]],
+            output: 0
         }];
     });
 
--- src/target_offer/min_in_rotate_array.js
+++ src/target_offer/min_in_rotate_array.js
@@ -7,12 +7,32 @@ function minInRotateArray(nums) {
     let startPos = 0;
     let endPos = nums.length-1;
 
-    while(startPos <= endPos) {
+    while(startPos < endPos) {
         const midPos = ((endPos-startPos) >> 1) + startPos;
 
-        if (nums[midPos] >= nums[endPos]) {
-            startPos = midPos+1;
+        // 必须先判断midPos是否落到后半部分因为：
+        // 1. 递增数组也是旋转数组，旋转量为0
+        // 2. 旋转量非0的旋转数组在循环的过程中也会落到后半部分
+        if (nums[midPos] < nums[endPos]) {
+            endPos = midPos;
+        } else if (nums[startPos] < nums[midPos]) {
+            startPos = midPos + 1;
+        // nums[startPos] === nums[endPos] === nums[midPos]情况无法判断midPos落在哪一部分
+        // 此时必须回退到顺序查找，确定midPos落入的区域
         } else {
+            for (let i = midPos+1; i <= endPos; i++) {
+                if (nums[i] === nums[midPos]) {
+                    continue;
+                    // 若后面的元素值增大，则说明落入前半区域
+                } else if (nums[i] > nums[midPos]) {
+                    startPos = i;
+                    break;
+                    // 若后面的元素减小，则说明可以直接
+                } else if (nums[i] < nums[midPos]) {
+                    return nums[i];
+                }
+            }
+
             endPos = midPos;
         }
     }
